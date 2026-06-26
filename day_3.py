2ba33f8bed097363ea92dc666a31bba5f94f534e
rafaydev01 <abdulrafaysaleem203@gmail.com>
2026-06-26 19:02:01 +0500

    Day 3 complete - Lesson 1 basics

---
+++ day_3.py
@@ -0,0 +1,18 @@
+# My Python Code - Lesson 1
+# Topic: Print, Variables aur Input
+
+# 1. Screen par print karna
+print("Hello World")
+
+# 2. Variables banana
+name = "Abdul Rafay"
+age = 20
+
+print(name)
+print(age)
+
+# 3. User se input lena
+user_name = input("enter your name: ")
+print("Welcome")
+print(user_name)
+

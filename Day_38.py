799922027c176c48930751c3277ec0aa5313e880
rafaydev01 <abdulrafaysaleem203@gmail.com>
2026-08-01 18:24:42 +0500

    Day 38 practice

---
+++ Day_38.py
@@ -0,0 +1,21 @@
+str = "my name is rafay"
+print(str.endswith("ay"))
+
+str = "i am a boy"
+print(str.capitalize())
+
+
+str = "i am hacker "
+print(str.replace("hacker" , "ethical hacker"))
+
+
+name = input("enter your name :")
+
+print(len(name))
+
+str = "Hi$ , hy$ , name $"
+print(str.count("$"))
+
+age = 20
+if(age >= 18):
+    print("rafay")

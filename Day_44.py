f763c45c91ca5f721bf460497669bbc956d95c60
rafaydev01 <abdulrafaysaleem203@gmail.com>
2026-08-07 16:51:16 +0500

    Only practice

---
+++ Day_44.py
@@ -0,0 +1,14 @@
+# Printing simple data
+print("Hello World")
+print("My name is Shradha.")
+
+# Variables and data types
+name = "Shradha"      # String (str)
+age = 25              # Integer (int)
+price = 99.99         # Float
+is_following = True   # Boolean (bool)
+a = None              # NoneType
+
+# Checking data types
+print(type(name))     # Output: <class 'str'>
+print(type(age))      # Output: <class 'int'>

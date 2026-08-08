5a743c82d089575156de134a397a02d11d841933
rafaydev01 <abdulrafaysaleem203@gmail.com>
2026-08-08 17:25:23 +0500

    Lists and Tuples

---
+++ Day_45.py
@@ -0,0 +1,28 @@
+marks = [96.8,67.9,73.3,87.3]
+print(marks)
+print(type(marks))
+
+marks = [67.8,98.8,98.8,89.8,78.9]
+print(marks[0:4])
+
+list = [2,1,3]
+list.append(4)
+print(list)
+
+
+list = [1,3,2,4]
+print(list.sort())
+print(list)
+
+list = [1,2,3,4,5]
+print(list.sort(reverse=True))
+print(list)
+
+
+list = [2,3,4,5,6]
+print(list.reverse())
+print(list)
+
+list = [1,2,3,4,5,6]
+list.insert (3,6)
+print(list)

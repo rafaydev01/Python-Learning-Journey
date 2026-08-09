6b63b8aa05e32285aa535363d5a75c7138ba0c96
rafaydev01 <abdulrafaysaleem203@gmail.com>
2026-08-09 23:15:49 +0500

    Sorry for this

---
+++ Day_46.py
@@ -0,0 +1,30 @@
+marks = [96.8,67.9,73.3,87.3]
+print(marks)
+print(type(marks))
+
+marks = [67.8,98.8,98.8,89.8,78.9]
+print(marks[0:4])
+
+list = [2,1,3]
+list.append(4)
+print(list)
+
+
+list = [1,3,2,4]
+print(list.sort())
+print(list)
+
+list = [1,2,3,4,5]
+print(list.sort(reverse=True))
+print(list)
+
+
+list = [2,3,4,5,6]
+print(list.reverse())
+print(list)
+
+list = [1,2,3,4,5,6]
+list.insert (3,6)
+print(list)
+
+name = "github ID vewiers please sorry for this i am reallly sorry i really try my best but i don't know why ! i am really serious in coding but this is only my mistake i want to make my future bright and rich but their is a big problem so please forgive me!"
